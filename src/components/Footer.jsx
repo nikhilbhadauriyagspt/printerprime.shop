42d93112989f76498053271fd16caed10fab4b51
mayankchikaragspt-boop <mayankchikara.gspt@gmail.com>
2026-03-14 05:19:56 +0530

    back to normal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -200,6 +200,13 @@ export default function Footer() {
           </div>
         </div>
 
+        {/* --- DISCLAIMER --- */}
+        <div className="mt-12 pt-8 border-t border-slate-100 flex justify-center">
+          <p className="text-[14px] font-black capitalize  text-slate-400 text-center max-w-2xl ">
+          Disclaimer - Theoretical guidance only. No software installation or distribution.
+          </p>
+        </div>
+
       </div>
     </footer>
   );
